e6a3c84fb7dddb94eed0e55cba94f100db970035
sampleghuser_rm4 <sampleghuser@MngEnvMCAP455753.onmicrosoft.com>
2025-10-20 20:53:35 +0200

    feat: Update descriptions in avm_knowledge.json and enhance transformation_action in ResourceConversionPlan

--- main.py
+++ main.py
@@ -202,13 +202,11 @@ class TerraformAVMOrchestrator:
             modules_details.append(module_detail)
             # self._log_agent_response(f"AVMResourceDetailsAgent - {mapping.target_module.name} {mapping.target_module.version}", modules_details)
 
-        with open(f"{output_dir}/05_avm_module_details.json", "w", encoding="utf-8") as f:
+        with open(f"{output_dir}/05_avm_modules_details.json", "w", encoding="utf-8") as f:
             f.write(json.dumps([v.model_dump() for v in modules_details], indent=2))
         
 
-        exit()
-
-        self.logger.info("Step 5: Running Converter Planning Agent Per Resource")
+        self.logger.info("Step 6: Running Converter Planning Agent Per Resource")
         resource_planning_agent = await ResourceConverterPlanningAgent.create()
 
         resources_planings = []
@@ -260,7 +258,7 @@ class TerraformAVMOrchestrator:
             resource_identifier = f"{mapping_result.source_resource.type}_{mapping_result.source_resource.name}"
             
             # Save structured JSON
-            with open(f"{output_dir}/05_{resource_identifier}_conversion_plan.json", "w", encoding="utf-8") as f:
+            with open(f"{output_dir}/06_{resource_identifier}_conversion_plan.json", "w", encoding="utf-8") as f:
                 planning_result_json = full_planning_result.model_dump_json(indent=2)
                 f.write(planning_result_json)
                        
